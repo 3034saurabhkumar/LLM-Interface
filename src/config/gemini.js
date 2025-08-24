bdb3026422bc6ed96a6c0bd55d3342274b2c57c1
Saurabh Kumar <saurabhkumar3034@gmail.com>
2025-08-24 11:41:51 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -5,7 +5,7 @@ import {
 } from "@google/generative-ai"
 
 const MODEL_NAME = "gemini-2.5-pro";
-const API_KEY = process.env.VITE_API_KEY;
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 async function runChat(prompt) {
   const genAI = new GoogleGenerativeAI(API_KEY);
